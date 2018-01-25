342364e1a8425ff3ae08ec59bb80354409ab780f
ÇAĞRI <33495266+cgrozn@users.noreply.github.com>
2018-01-26 01:56:57 +0300

    Add files via upload

---
+++ sqlbaglantisi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public class sqlbaglantisi
+{
+    public SqlConnection baglan()
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=CAGRI; Initial Catalog=siteDB" + "; Integrated Security=true ");
+        baglanti.Open();
+        SqlConnection.ClearPool(baglanti);
+        SqlConnection.ClearAllPools();
+        return (baglanti);
+
+
+
+    }
+
+
+}
